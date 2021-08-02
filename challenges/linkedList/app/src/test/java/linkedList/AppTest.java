e022a90d1c7aaad0f733ced02cbaeb22deaf67e5
nooraldeen97 <noor-aldeen97@hotmail.com>
2021-08-02 23:24:52 +0300

    ed

--- challenges/linkedList/app/src/test/java/linkedList/AppTest.java
+++ challenges/linkedList/app/src/test/java/linkedList/AppTest.java
@@ -48,10 +48,7 @@ public class AppTest {
       assertEquals("make sure that head point to the firstNode",2, ls.head.next.value);
 
     }
-    // Where k is greater than the length of the linked list
-  @Test public void TestWhenKgreater(){
-      ls.
-  }
+
 
     @Test public void TestInsertBefore(){
       ls.Insert(1);
